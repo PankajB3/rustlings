af485c7aee1f387bd2173b1ffee6de47cad5b1fe
PankajB3 <pankajbansal@rapidinnovation.dev>
2023-02-21 14:55:48 +0530

    modules

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -3,12 +3,11 @@
 // 'use' and 'as' keywords. Fix these 'use' statements to make the code compile.
 // Execute `rustlings hint modules2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    // use self::fruits::PEAR;
+    // use self::veggies::CUCUMBER;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
@@ -19,6 +18,8 @@ mod delicious_snacks {
         pub const CUCUMBER: &'static str = "Cucumber";
         pub const CARROT: &'static str = "Carrot";
     }
+    pub const fruit:&str = fruits::PEAR;
+    pub const veggie:&str = veggies::CUCUMBER;
 }
 
 fn main() {
